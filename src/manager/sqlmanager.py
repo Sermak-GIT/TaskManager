dd79b8fd5804d636332f55c5fe44ee57cceaf7b7
Sermak <egidius.mysliwietz@gmx.de>
2017-12-08 00:01:18 +0100

    Added methods to SQL Manager

--- src/manager/sqlmanager.py
+++ src/manager/sqlmanager.py
@@ -3,25 +3,32 @@
 
 import sqlite3 as lite
 import sys
+import logging
+import pprint
 
-cars = (
-    (1, 'Audi', 52642),
-    (2, 'Mercedes', 57127),
-    (3, 'Skoda', 9000),
-    (4, 'Volvo', 29000),
-    (5, 'Bentley', 350000),
-    (6, 'Hummer', 41400),
-    (7, 'Volkswagen', 21600)
-)
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 
 def issue_new_id():
+    logging.debug("issue_new_id() called")
+    i = 0
+    while is_id_in_use(i):
+        i += 1
+        is_id_in_use(i)
+    logging.debug("issue_new_id() issued %i", i)
+    return i
+
+
+def is_id_in_use(to_check_id):
+    logging.debug("is_id_in_use() checks id " + to_check_id.__str__())
     con = lite.connect("taskmanager.db")
     with con:
-        ret_id = con.cursor().lastrowid
+        ret_id = con.cursor().execute("SELECT * FROM Entries WHERE Id = " + to_check_id.__str__()).fetchone()
         if ret_id is None:
-            return 0
-        return ret_id + 1
+            logging.debug("ID " + to_check_id.__str__() + " not in use")
+            return False
+        logging.debug("ID " + to_check_id.__str__() + " already in use")
+        return True
 
 
 def init():
@@ -31,13 +38,89 @@ def init():
         try:
             cursor.execute("CREATE TABLE Entries(Id INT, NextAction TEXT, Notes INT, Icon BLOB, Deadline DATE, "
                            "Time TIME, Setting TEXT, Willpower INT, Audio INT, Prio INT)")
+            logging.info("Table Entries created")
         except lite.OperationalError:
-            print("Table found")
+            logging.info("Table Entries found")
+
+
+def add_entry(entry):
+    logging.info("Adding entry: " + entry.__str__())
+    connection = lite.connect("taskmanager.db")
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Entries VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", entry)
+
+
+def read_entry(entryid):
+    logging.info("Reading entry: " + entryid.__str__())
+    con = lite.connect('taskmanager.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Entries WHERE Id=" + entryid.__str__())
+        rows = cur.fetchall()
+        try:
+            logging.info("Reading entry: " + entryid.__str__() + " successful")
+            return rows[0]
+        except IndexError:
+            logging.info("Entry: " + entryid.__str__() + " does not exist")
+            return None
+
+
+def update_entry(entry):
+    entryid = entry[0]
+    entry = entry[1:]
+    logging.info("Updating entry: " + entryid.__str__())
+    con = lite.connect('taskmanager.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE Entries "
+                    "SET NextAction=?, Notes = ?, Icon=?, Deadline=?, Time=?, "
+                    "Setting=?, Willpower=?, Audio=?, Prio=? "
+                    "WHERE Id = " + entryid.__str__(), entry)
+        cur.fetchall()
+
+
+def get_all_entries():
+    logging.info("Reading all entries")
+    con = lite.connect('taskmanager.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Entries")
+        rows = cur.fetchall()
+        try:
+            logging.info("Reading all entries successful")
+            return rows
+        except IndexError:
+            logging.info("There are no entries")
+            return None
+
+
+def delete_entry(entryid):
+    logging.info("Deleting entry " + entryid.__str__())
+    con = lite.connect('taskmanager.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM Entries WHERE Id=" + entryid.__str__())
+        rows = cur.fetchall()
+
+
+def delete_everything():
+    logging.warning("Deleting EVERYTHING")
+    con = lite.connect('taskmanager.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("DELETE FROM Entries")
+        rows = cur.fetchall()
 
 
 init()
 
-print(issue_new_id())
+pprint.pprint(read_entry(1))
+update_entry((1, 'Do #3', 'Hello', 324, '23.03.12', '23:22', 'pc', 100, 0, 12))
+pprint.pprint(read_entry(1))
+delete_everything()
+pprint.pprint(read_entry(1))
+
 '''
 cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
 cur.executemany("INSERT INTO Cars VALUES(?, ?, ?)", cars)
